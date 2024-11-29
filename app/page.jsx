2eea9c3e145ffee36b12d7b219cdccabcde6d0f2
kerloshany3 <kerlostablet@gmail.com>
2024-11-29 13:49:20 +0200

    lamp addons

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import {LampDemo} from './components/Banner'
+
+export default function Home() {
+  return (
+  
+    
+    <div>
+
+
+      <LampDemo></LampDemo>
+
+      
+    </div>
+  );
+}
